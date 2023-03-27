f895fff9fc5d3ffd0f5d193533c2f7ccaf7b9147
bon831 <wenziqin01@gmail.com>
2023-03-27 15:20:16 +0800

    variable, array, object

---
+++ index.js
@@ -0,0 +1,53 @@
+// let data1 = 1
+// let data2 = "1"
+// let data3 = "1.25"
+// data5 = true
+// console.log(data1, typeof data1)
+// console.log(data2, typeof data2)
+
+// // let data3 = data1 + data2
+// // console.log(data3, typeof data3)
+// // let data4 = data1 + parseInt(data2)
+// // console.log(data3, typeof data3)
+// let data4 = data1 + parseFloat(data3)
+// console.log(data4, typeof data4)
+
+// let data3 = data1
+
+
+// Array
+// let data = ["one", 2, true]
+// console.log(data[0], typeof data[0])
+
+// Object
+// let user = {
+//     name: "Fucking BON",
+//     faculty: "FKEKK",
+//     phone: "00000",
+//     email: "ziqin@utem.edu.my"
+// }
+// console.log(user.faculty)
+// console.log(user.name)
+
+// let user = {
+//     name: "Fucking BON",
+//     faculty: "FKEKK",
+//     phone: [
+//         "00000",
+//         "11111"
+//     ],
+//     email: "ziqin@utem.edu.my"
+// }
+// console.log(user.phone[1])
+
+let user = {
+    name: "Fucking BON",
+    faculty: "FKEKK",
+    phone: {
+        office: "00000",
+        mobile: "11111"
+    },
+    email: "ziqin@utem.edu.my"
+}
+console.log(user.phone.office)
+
